b82f0bc7a9d727d9cd7e2e2463a64d58f46b5e43
leoosilvp <leo965679@gmail.com>
2025-09-04 11:43:29 -0300

    colocando texto

--- src/components/CalcularFrete.jsx
+++ src/components/CalcularFrete.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
 
 const CalcularFrete = () => {
   return (
-    <div>CalcularFrete</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-600 p-4">
+      <div className="bg-white p-8 rounded-2xl max-w-md text-center">
+        <h1 className="text-2xl font-bold text-blue-700 mb-6">Calculadora de frete</h1>
+      </div>
+    </div>
   )
 }
 
